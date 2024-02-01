676a928dc368b62cd69710d03b07513f66972cb4
kostyauzb <qosimjonomonovko@gmail.com>
2024-02-02 02:36:40 +0500

    problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Modal1 from "./components/Modal1";
 import Modal2 from "./components/Modal2";
-import Modal3 from "./components/modal3";
+import Modal3 from "./components/Modal3";
 const App = () => {
   return (
     <div>
